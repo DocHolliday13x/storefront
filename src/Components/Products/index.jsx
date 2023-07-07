764f0be4ef10d9f55528b56e95a980f4b6d4fa11
Ryan Eastman <rking1323@gmail.com>
2023-07-06 21:49:28 -0700

    i'm throwin errors left and right, i'm exhuasted and not fixing this tonight. Submit for credit

--- src/Components/Products/index.jsx
+++ src/Components/Products/index.jsx
@@ -1,51 +1,46 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography, Grid } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, CardMedia, Typography, Grid } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-});
+// import { Link } from 'react-router-dom';
+// import '../../App.css';
 
 function Products() {
-  const classes = useStyles();
-  const products = useSelector(state => state.products);
+    const { products } = useSelector(state => state.ProductsReducer);
+    console.log('products', products);
 
-  return (
-    <Grid container spacing={4}>
-      {products.map(product => (
-        <Grid item key={product.id} xs={12} sm={6} md={4}>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.cardMedia}
-              image={product.image}
-              title={product.title}
-            />
-            <CardContent className={classes.cardContent}>
-              <Typography gutterBottom variant="h5" component="h2">
-                {product.title}
-              </Typography>
-              <Typography>
-                {product.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary" component={Link} to={`/products/${product.id}`}>
-                View
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      ))}
-    </Grid>
-  );
+    return (
+        products.map((product, index) => (
+            <>
+                {
+                    <Grid className="product" item xs={12} sm={6} md={4} lg={3} key={`product-${index}`}>
+                        <Card variant="outlined" sx={{ maxWidth: 345 }}>
+                          <CardMedia id="product-image" 
+                            component="img" 
+                            height="140" 
+                            title={product.name} 
+                            image={`https://source.unsplash.com/random/?${product.name}}`}
+                            alt={product.title}
+                          />
+                          <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                              Name: {product.name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                              Price: ${product.price}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                              In Stock: {product.inStock}
+                            </Typography>
+                          </CardContent>
+                          <CardActions>
+                            <Button size="small">Add to Cart</Button>
+                            <Button size="small">View Details</Button>
+                          </CardActions>
+                        </Card>
+                      </Grid>
+                }
+            </>
+        ))
+    )
 }
 
 
